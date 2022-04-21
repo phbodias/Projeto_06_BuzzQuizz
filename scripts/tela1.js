5b0562009e67c97689e0c2748f6b5e6e7bd9552a
Pedro Dias <pedrohenriquebritooliveira@gmail.com>
2022-04-21 11:45:50 -0300

    importar quizzes

--- scripts/tela1.js
+++ scripts/tela1.js
@@ -1,5 +1,21 @@
 function obterQuizz(){
     const obter = axios.get("https://mock-api.driven.com.br/api/v6/buzzquizz/quizzes");
+    obter.then(sucess);
+}
+
+function sucess(resposta){
+    let data = resposta.data;
+    console.log(data[0]);
+    const quizzes = document.querySelector(".quizzes");
+    for (let i = 0; i < data.length; i++){
+        quizzes.innerHTML += `
+            <div class="quizz">
+                <img src="${data[i].image}" alt="">
+                <p class="legenda">${data[i].title}</p>
+            </div>
+        `
+    }
 }
 
 obterQuizz();
+
